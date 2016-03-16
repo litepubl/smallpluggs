f381146211da8276c52d4ecd48ac71edebc53a1f
litepubl <yarrowsoft@gmail.com>
2016-03-16 22:35:14 +0300

    added

---
+++ plugins/smallplugs-scrollablecats/scrollcats.plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Lite Publisher
+ * Copyright (C) 2010 - 2016 Vladimir Yushko http://litepublisher.com/ http://litepublisher.ru/
+ * Licensed under the MIT (LICENSE.txt) license.
+ *
+ */
+
+class scrollcats extends tplugin {
+
+  public static function i() {
+    return getinstance(__class__);
+  }
+
+  public function install() {
+tplugins::i()->add('enscroll');
+    $plugindir = basename(dirname(__file__));
+tthemeparser::i()->addtags("plugins/$plugindir/resource/theme.categories.txt", false);
+  }
+
+  public function uninstall() {
+    $plugindir = basename(dirname(__file__));
+tthemeparser::i()->removetags("plugins/$plugindir/resource/theme.categories.txt", false);
+}
+
+}//class
