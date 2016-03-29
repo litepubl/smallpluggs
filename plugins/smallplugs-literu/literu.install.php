66c15a4bd969d14f1bda689e3a224b20d740e1ec
litepubl <yarrowsoft@gmail.com>
2016-03-30 00:25:53 +0300

    added

---
+++ plugins/smallplugs-literu/literu.install.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Lite Publisher
+ * Copyright (C) 2010 - 2016 Vladimir Yushko http://litepublisher.com/ http://litepublisher.ru/
+ * Licensed under the MIT (LICENSE.txt) license.
+ *
+ */
+
+namespace litepubl\smallplugs;
+use litepubl;
+
+function literuInstall($self) {
+  \tmenus::i()->oncontent = $self->onMenuContent;
+\tbackuper::i()->onuploaded = $this->onuploaded;
+}
+
+function literuUninstall($self) {
+  \tmenus::i()->unbind($self);
+  \tbackuper::i()->unbind($self);
+}
---
+++ plugins/smallplugs-literu/literu.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Lite Publisher
+ * Copyright (C) 2010 - 2016 Vladimir Yushko http://litepublisher.com/ http://litepublisher.ru/
+ * Licensed under the MIT (LICENSE.txt) license.
+ *
+ */
+
+namespace litepubl\smallplugs;
+use litepubl;
+
+class literu extends \tplugin {
+
+  public function onMenuContent($menu, &$content) {
+    $content = \ttheme::i()->parse($content);
+  }
+
+public function onuploaded() {
+$links = \tlinkswidget::i();
+foreach ($links->items as $id => $item) {
+if (\strbegin($item['url'], 'https://github.com/litepubl/cms/archive/')) {
+$links->items[$id]['url'] = sprintf('https://github.com/litepubl/cms/archive/v%s.zip', litepublisher::$options->version);
+$links->save();
+return;
+}
+}
+}
+  
+}//class
