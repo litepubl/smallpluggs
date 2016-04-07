4a8573c8190a1c787e7844f99d345ffa5fd860d6
litepubl <yarrowsoft@gmail.com>
2016-04-08 00:17:07 +0300

    prefix

--- plugins/smallplugs_enscroll/enscroll.plugin.php
+++ plugins/smallplugs_enscroll/enscroll.plugin.php
@@ -7,38 +7,38 @@
  */
 
 namespace litepubl\plugins\smallplugs_enscroll;
-use litepubl\tplugins;
+use litepubl\tplugin;
 use litepubl\tjsmerger;
 use litepubl\tcssmerger;
 
 class enscroll extends tplugin {
 
-  public function install() {
-    $plugindir = basename(dirname(__file__));
-    $js = tjsmerger::i();
-    $js->lock();
-    $js->add('default', "plugins/$plugindir/resource/enscroll-0.6.1.min.js");
-    $js->add('default', "plugins/$plugindir/resource/init.js");
-    $js->unlock();
+    public function install() {
+        $plugindir = basename(dirname(__file__));
+        $js = tjsmerger::i();
+        $js->lock();
+        $js->add('default', "plugins/$plugindir/resource/enscroll-0.6.1.min.js");
+        $js->add('default', "plugins/$plugindir/resource/init.js");
+        $js->unlock();
 
-    $css = tcssmerger::i();
-    $css->lock();
-    $css->add('default', "plugins/$plugindir/resource/enscroll.min.css");
-    $css->unlock();
-  }
+        $css = tcssmerger::i();
+        $css->lock();
+        $css->add('default', "plugins/$plugindir/resource/enscroll.min.css");
+        $css->unlock();
+    }
 
-  public function uninstall() {
-    $plugindir = basename(dirname(__file__));
-    $js = tjsmerger::i();
-    $js->lock();
-    $js->deletefile('default', "plugins/$plugindir/resource/enscroll-0.6.1.min.js");
-    $js->deletefile('default', "plugins/$plugindir/resource/init.js");
-    $js->unlock();
+    public function uninstall() {
+        $plugindir = basename(dirname(__file__));
+        $js = tjsmerger::i();
+        $js->lock();
+        $js->deletefile('default', "plugins/$plugindir/resource/enscroll-0.6.1.min.js");
+        $js->deletefile('default', "plugins/$plugindir/resource/init.js");
+        $js->unlock();
 
-    $css = tcssmerger::i();
-    $css->lock();
-    $css->deletefile('default', "plugins/$plugindir/resource/enscroll.min.css");
-    $css->unlock();
-  }
+        $css = tcssmerger::i();
+        $css->lock();
+        $css->deletefile('default', "plugins/$plugindir/resource/enscroll.min.css");
+        $css->unlock();
+    }
 
 } //class
